3ba3b7b9f825968051733552bd676e029ec65a40
Sophia Anopa <sanopa@gmail.com>
2014-09-12 21:13:36 -0400

    Initial Commit

---
+++ whyl/YOHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YOHAppDelegate.h
+//  whyl
+//
+//  Created by Sophia Anopa on 12.09.14.
+//  Copyright (c) 2014 Aiiyoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YOHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ whyl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  whyl
+//
+//  Created by Sophia Anopa on 12.09.14.
+//  Copyright (c) 2014 Aiiyoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YOHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YOHAppDelegate class]));
+    }
+}
