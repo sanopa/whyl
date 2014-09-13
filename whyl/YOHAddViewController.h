56f1d1d9f82e0b5b707e916d9b9eb7a1052a66d8
Sophia Anopa <sanopa@gmail.com>
2014-09-12 21:28:08 -0400

    Initial view controller setup.

---
+++ whyl/YOHAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  YOHAddViewController.h
+//  whyl
+//
+//  Created by Sophia Anopa on 12.09.14.
+//  Copyright (c) 2014 Aiiyoh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YOHAddViewController : UIViewController
+
+@end
---
+++ whyl/YOHPostViewController.m
@@ -0,0 +1,49 @@
+//
+//  YOHPostViewController.m
+//  whyl
+//
+//  Created by Sophia Anopa on 12.09.14.
+//  Copyright (c) 2014 Aiiyoh. All rights reserved.
+//
+
+#import "YOHPostViewController.h"
+
+@interface YOHPostViewController ()
+
+@end
+
+@implementation YOHPostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
